00d5abe5f0f74d85fd919c388d7bec0d0b994bbf
debuggerdev24 <debuggerdev.dds@gmail.com>
2026-01-12 14:36:30 +0530

    set up frontend and build registerScreen and try to apply routing

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
-  console.log("server runnign on port", PORT)
+  console.log("server running on port", PORT)
 );
 
 process.on("unhandledRejection", (err, promise) => {
